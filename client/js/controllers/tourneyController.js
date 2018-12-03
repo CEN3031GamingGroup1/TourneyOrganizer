756880adcafc3413fb93b4f0cb1521b283ca870a
Christian Sarmiento <christiansarmiento@Christians-MacBook-Pro.local>
2018-12-02 19:01:20 -0500

    index angular finished

--- client/js/controllers/tourneyController.js
+++ client/js/controllers/tourneyController.js
@@ -26,91 +26,91 @@ function($scope, $cookies, Tourneys) {
 		$scope.newListing = {};
 		Listings.create($scope.listings.listings).then(function(response){
 		$scope.listings = response.data;
-	}, function(error) {
-	console.log('Unable to create listings:', error);
-});
-*/
-const newDate = new Date($scope.tournamentDate.year, $scope.tournamentDate.month-1, $scope.tournamentDate.day);
-var newTourney = {
-	tournamentName : $scope.tournamentName,
-	game : $scope.game,
-	address : $scope.address,
-	tournamentDate : newDate,
-	address : $scope.address,
-	details: $scope.details,
-	fee : $scope.fee,
-	featured : 0,
-	ageReq : $scope.ageReq
-};
-Tourneys.create(newTourney).then(function(response){
-	Tourneys.getAll().then(function(response) {
-		$scope.tourneys = response.data; //"redirecting" or updating the table again
+		}, function(error) {
+		console.log('Unable to create listings:', error);
+		});
+		*/
+		const newDate = new Date($scope.tournamentDate.year, $scope.tournamentDate.month-1, $scope.tournamentDate.day);
+		var newTourney = {
+			tournamentName : $scope.tournamentName,
+			game : $scope.game,
+			address : $scope.address,
+			tournamentDate : newDate,
+			address : $scope.address,
+			details: $scope.details,
+			fee : $scope.fee,
+			featured : 0,
+			ageReq : $scope.ageReq
+		};
+		Tourneys.create(newTourney).then(function(response){
+			Tourneys.getAll().then(function(response) {
+				$scope.tourneys = response.data; //"redirecting" or updating the table again
 
-	}, function(error) {
-		console.log('Unable to retrieve tourneys:', error);
-	});
-}, function(err) {
-	console.log('Could not create new tourney:', err);
-});
-};
+			}, function(error) {
+				console.log('Unable to retrieve tourneys:', error);
+			});
+		}, function(err) {
+			console.log('Could not create new tourney:', err);
+		});
+	};
 
-$scope.deleteTourney = function(index) {
-	/**TODO
-	Delete the article using the Listings factory. If the removal is successful,
-	navigate back to 'listing.list'. Otherwise, display the error.
-	*/
-	Tourneys.delete($scope.tourneys[index]._id).then(function(response) {
+	$scope.deleteTourney = function(index) {
 		/**TODO
 		Delete the article using the Listings factory. If the removal is successful,
 		navigate back to 'listing.list'. Otherwise, display the error.
 		*/
-		Tourneys.getAll().then(function(response) {
-			$scope.tourneys = response.data; //"redirecting" or updating the table again
-		}, function(error) {
-			console.log('Unable to retrieve tourney:', error);
-		});
-	}, function(err) {
-		console.log('Could not delete tourney:', err);
-	})
-};
+		Tourneys.delete($scope.tourneys[index]._id).then(function(response) {
+			/**TODO
+			Delete the article using the Listings factory. If the removal is successful,
+			navigate back to 'listing.list'. Otherwise, display the error.
+			*/
+			Tourneys.getAll().then(function(response) {
+				$scope.tourneys = response.data; //"redirecting" or updating the table again
+			}, function(error) {
+				console.log('Unable to retrieve tourney:', error);
+			});
+		}, function(err) {
+			console.log('Could not delete tourney:', err);
+		})
+	};
 
-$scope.featureTourney = function(index) {
-	$scope.tourneys[index].featured = 1;
-	Tourneys.update($scope.tourneys[index]).then(function(response) {
-		Tourneys.getAll().then(function(response) {
-			$scope.tourneys = response.data; //"redirecting" or updating the table again
-		}, function(error) {
-			console.log('Unable to retrieve tourney:', error);
-		});
-	}, function(err) {
-		console.log('Could not delete tourney:', err);
-	})
-};
+	$scope.featureTourney = function(index) {
+		$scope.tourneys[index].featured = 1;
+		Tourneys.update($scope.tourneys[index]).then(function(response) {
+			Tourneys.getAll().then(function(response) {
+				$scope.tourneys = response.data; //"redirecting" or updating the table again
+			}, function(error) {
+				console.log('Unable to retrieve tourney:', error);
+			});
+		}, function(err) {
+			console.log('Could not delete tourney:', err);
+		})
+	};
 
-$scope.showDetails = function(tourney) {
-	$scope.detailedInfo = tourney;
-};
-
-
-$scope.sendCookies = function(id) {
-	$cookies.put('id', id);
-};
-
-$scope.getCookies = function() {
-	console.log($cookies.get('id'));
-	return $cookies.get('id');
-};
+	$scope.showDetails = function(tourney) {
+		$scope.detailedInfo = tourney;
+	};
 
 
-$scope.displayDate = function(tourney) {
-	var newDate = new Date;
-	try {
-		newDate = new Date(tourney.tournamentDate);
+	$scope.sendCookies = function(id) {
+		$cookies.put('id', id);
+	};
+
+	$scope.getCookies = function() {
+		console.log($cookies.get('id'));
+		return $cookies.get('id');
+	};
+
+
+	$scope.displayDate = function(tourney) {
+		var newDate = new Date;
+		try {
+			newDate = new Date(tourney.tournamentDate);
+		}
+		catch(err) {
+			return '';
+		}
+		return newDate.toLocaleDateString();
 	}
-	catch(err) {
-		return '';
-	}
-	return newDate.toLocaleDateString();
-}
 }
 ]);
