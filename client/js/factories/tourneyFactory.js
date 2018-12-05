1ea2547f07e87d86912c4f9d96b36bdc2d34e335
Jared Smith <jrdsmi4123@gmail.com>
2018-12-05 12:41:34 -0500

    can connect to db

--- client/js/factories/tourneyFactory.js
+++ client/js/factories/tourneyFactory.js
@@ -1,19 +1,19 @@
 angular.module('tourneys', []).factory('Tourneys', function ($http) {
 	var methods = {
 		getAll: function () {
-			return $http.get('http://localhost:8080/api/tourneys');
+			return $http.get('/api/tourneys');
 		},
 
 		create: function (tourney) {
-			return $http.post('http://localhost:8080/api/tourneys', tourney);
+			return $http.post('/api/tourneys', tourney);
 		},
 
 		update: function (tourney) {
-			return $http.put('http://localhost:8080/api/tourneys/' + tourney._id, tourney);
+			return $http.put('/api/tourneys/' + tourney._id, tourney);
 		},
 
 		delete: function (id) {
-			return $http.delete('http://localhost:8080/api/tourneys/' + id);
+			return $http.delete('/api/tourneys/' + id);
 		}
 	};
 
--- client/js/factories/userFactory.js
+++ client/js/factories/userFactory.js
@@ -1,29 +1,29 @@
 angular.module('users', []).factory('Users', function ($http) {
 	var methods = {
 		getAll: function () {
-			return $http.get('http://localhost:8080/api/users');
+			return $http.get('/api/users');
 		},
 
 		create: function(user) {
-      return $http.post('http://localhost:8080/signup', user);
+      return $http.post('/signup', user);
     },
 
 		loginn: function(user) {
-      return $http.post('http://localhost:8080/login', user);
+      return $http.post('/login', user);
 		},
 
 		update: function(id, user) {
-      return $http.put('http://localhost:8080/api/users/'+ id, user);
+      return $http.put('/api/users/'+ id, user);
 		},
 
 		getUsername: function() {
-      return $http.get('http://localhost:8080/getUsername');
+      return $http.get('/getUsername');
 		},
 		getUser: function(username) {
-			return $http.get('http://localhost:8080/api/users/username/' + username, username);
+			return $http.get('/api/users/username/' + username, username);
 		},
     delete: function(id) {
-      return $http.delete('http://localhost:8080/api/users/' + id);
+      return $http.delete('/api/users/' + id);
     }
   };
   return methods;
