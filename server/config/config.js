894f545c99d06636c4dea63b9649f9171ebce26a
Jared Smith <jrdsmi4123@gmail.com>
2018-12-05 16:54:16 -0500

    PORT

--- server/config/config.js
+++ server/config/config.js
@@ -2,6 +2,6 @@ module.exports = {
 	db: {
 		uri: 'mongodb://andy:abc1234@ds237735.mlab.com:37735/gtourney',
 	},
-	port: 8080
+	port: process.env.PORT || 8080
 	// BEFORE: port: process.env.PORT || 8080 -- After: ^^
 };
